f4579bb9bca13dd9d79fcc38f3e3975e4b3b4365
Geovane Borba <geovanne@live.com>
2024-04-26 17:00:46 -0300

    test(mock:restaurant): implement mock for getManagedRestaurant

---
+++ test/mocks/api/restaurant/get-managed-restaurant-mock.ts
@@ -0,0 +1,17 @@
+import { HttpResponse, http } from "msw"
+import { GetManagedRestaurantResponse } from "../../../../src/api/restaurant/get-managed-restaurant"
+
+export const getManagedRestaurantMock = http.get<
+  never,
+  never,
+  GetManagedRestaurantResponse
+>("/managed-restaurant", () => {
+  return HttpResponse.json({
+    id: "custom-restaurant-id",
+    name: "Pizza Shop",
+    description: "Custom restaurant description.",
+    managerId: "custom-user-id",
+    createdAt: new Date(),
+    updatedAt: null,
+  })
+})
